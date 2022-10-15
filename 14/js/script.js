d63371b4623427a5158588fa8394dc097161e47f
Alisson Alves <alisson.a.m88@gmail.com>
2022-10-14 23:26:17 -0300

    Delete 14 directory

--- 14/js/script.js
+++
@@ -1,6 +0,0 @@
-let anos = parseInt(prompt("Anos fumando:"));
-let cig_Dia = parseInt(prompt("Cigarros por dia:"));
-let ca20 = parseFloat(prompt("Valor da carteira de cigarro:"))
-
-
-document.write("Dinheiro gasto: R$" + ((ca20 /20) * (cig_Dia * (anos * 365))));
